0c29e62b697caad98f1103931991c04adb83a8f6
John Muraski <john.t.muraski@outlook.com>
2024-06-04 16:43:24 -0500

    Init

---
+++ HammerSimAPI/HammerSimAPI/Models/Unit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HammerSimAPI.Models
+{
+    /// <summary>
+    /// Warhammer 40K Squad or character datasheet
+    /// </summary>
+    public class Unit
+    {
+        public string Name { get; set; }
+        public string Faction { get; set; }
+        public string SubFaction { get; set; }
+        public int ModelCount { get; set; } 
+
+    }
+}
