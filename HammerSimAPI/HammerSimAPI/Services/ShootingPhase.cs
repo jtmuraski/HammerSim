a8ab3c6de8d8c978b927bd8280d28e8d06e2e623
John Muraski <john.t.muraski@outlook.com>
2024-07-09 16:40:09 -0500

    Shooting Phase ready for intial testing

--- HammerSimAPI/HammerSimAPI/Services/ShootingPhase.cs
+++ HammerSimAPI/HammerSimAPI/Services/ShootingPhase.cs
@@ -1,5 +1,6 @@
 ﻿using HammerSimAPI.Models.Data;
 using HammerSimAPI.Models.Units;
+using HammerSimAPI.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,11 +27,17 @@ namespace HammerSimAPI.Services
                 RangedWeaponResult result = new RangedWeaponResult();
                 if(weapon.Range >= range)
                 {
-                    result.Hits = RollAttacks(attacker, defender, weapon, results);
+                    int modifier = 0;
+                    result.Hits = RollAttacks(attacker, defender, weapon, modifier, results);
+
                     int tempWounds = RollForWounds(attacker, weapon, defender, result.Hits);
-                    result.Saves = RollSaves(attacker, weapon, defender, result.Hits);
+
+                    result.Saves = RollSaves(attacker, weapon, defender, tempWounds);
+
                     result.Wounds = tempWounds - result.Saves;
+
                     result.ModelsKilled = CalculateModelsKilled(result.Wounds, defender);
+
                     results.IndividualWeaponResults.Add(result);
                 }
                 else
@@ -51,7 +58,7 @@ namespace HammerSimAPI.Services
 
             for(int i = 0; i < numOfAttacks; i++)
             {
-                currentRoll = RollD6();
+                currentRoll = Dice.RollD6();
 
                 if (currentRoll == 1)
                     misses++;
@@ -67,6 +74,77 @@ namespace HammerSimAPI.Services
 
             results.TotalShotsFired += numOfAttacks;
             return hits;
+        }
+
+        private static int RollForWounds(Unit attacker, RangedWeapon weapon, Unit defender, int hits)
+        {
+            int wounds = 0;
+            
+            for(int i = 0; i < hits; i++)
+            {
+                int currentRoll = Dice.RollD6();
+                int woundSuccessValue = 0;
+                if(weapon.Strength >= defender.Toughness * 2)
+                    woundSuccessValue = 2;
+                else if(weapon.Strength > defender.Toughness)
+                    woundSuccessValue = 3;
+                else if(weapon.Strength == defender.Toughness)
+                    woundSuccessValue = 4;
+                else if(weapon.Strength < defender.Toughness)
+                    woundSuccessValue = 5;
+                else
+                    woundSuccessValue = 6;
+
+                if(currentRoll == 6)
+                    wounds++;
+                else
+                    wounds += currentRoll >= woundSuccessValue ? 1 : 0;
+            }
+
+            return wounds;
+        }
+
+        private static int RollSaves(Unit attacker, RangedWeapon weapon, Unit defender, int wounds)
+        {
+            int saves = 0;
+
+            for(int i = 0; i < wounds;i++)
+            {
+                int currentRoll = Dice.RollD6();
+                if(currentRoll > 1)
+                {
+                    int modifiedRoll = currentRoll - weapon.AP;
+                    int saveValue = 0;
+                    if(defender.InvulnerableSave > 0)
+                    {
+                        saveValue = defender.InvulnerableSave < defender.Save ? defender.InvulnerableSave : defender.Save;
+                    }
+                    else
+                    {
+                        saveValue = defender.Save;
+                    }
+
+                    if (modifiedRoll >= saveValue)
+                    {
+                        saves++;
+                    }
+                }
+            }
+
+            return saves;
+        }
+
+        private static int CalculateModelsKilled(int wounds, Unit defender)
+        {
+            int modelsKilled = 0;
+            int woundCounter = wounds;
+            while(woundCounter >= defender.Wounds)
+            {
+                woundCounter -= defender.Wounds;
+                modelsKilled++;
+            }
+
+            return modelsKilled;
         }
     }
 }
---
+++ HammerSimAPI/HammerSimAPI/Utilities/Dice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HammerSimAPI.Utilities
+{
+    public static class Dice
+    {
+        public static int RollD6()
+        {
+            Random rand = new Random();
+            return rand.Next(1, 7);
+        }
+    }
+}
